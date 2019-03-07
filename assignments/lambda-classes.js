c3d2fc9e13a816f545b26c7b115df1cdb9a73b88
Mindy Allen <mrsmallen07@gmail.com>
2019-03-07 15:51:47 -0500

    finished prototype- added classes and extends, now working on the lambda classes portion

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,18 @@
 // CODE here for your Lambda Classes
+
+ class person extends baseclass{
+     constructor(attribute) {
+         this.name
+     }
+    }
+    speak() {
+        return `${this.name} says ${this.phrase} `
+    }
+
+
+class person = base-class({
+    name: 'Fred',
+    age: 37,
+    location: 'Bedrock',
+    gender: 'Male',
+ });
